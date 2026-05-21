da64b66da2bd494a4aa5e9ca2e93c7cb3db71a84
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-05-20 22:38:33 -0300

    Update Q20.py

--- Lista de Exercicios/Q20.py
+++ Lista de Exercicios/Q20.py
@@ -2,12 +2,13 @@
 
 
 # entrada nao tem
-
+print ("Irei achar os multiplos de um numero entre o intervalo de 1 ate 200")
+numero = int(input("Informe o numero: "))
 # processamento
 soma = 0
-for i in range(100,201):
-    if i % 7 == 0:
+for i in range(1,201):
+    if i % numero == 0:
         soma = soma + i
-        print(f' Multiplo de 7 - {i}')
+        print(f' Multiplo de {numero} - {i}')
         
-print(f'Soma dos multiplos de 7 entre 100 e 200 = {soma}')        
+print(f'Soma dos multiplos de {numero} entre 10 e 200 = {soma}')        
